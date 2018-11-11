a36db789b17fa077696b80ca00bde163289f8398
philipem <23695808+philipem@users.noreply.github.com>
2018-11-11 21:15:21 +0100

    added rows more dynamically

--- plot_bar.py
+++ plot_bar.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
-def plot_bars(noun_count):
+def plot_bars(noun_count, rows):
     plt.figure(1)
+    
+    factor = 1/rows 
+    
+    for i in noun_count.keys():
+        noun_count[i] = noun_count[i]*factor
+        
     plt.bar(list(noun_count.keys()), noun_count.values(), color='g')
